98680886a22c433ee6e29da0bde1ebb489002a12
xiaoliang.zuo <xiaoliang.zuo@ck-telecom.com>
2015-12-16 13:56:59 +0800

    newest

--- 01.c
+++ 01.c
@@ -1,4 +1,5 @@
 this is the first world in the master
 there is a change in the dev branch 
 this the third time to change
-10.26 test the origin 
+10.26 test the origin
+the newest cmmoit by shawn 12.16
